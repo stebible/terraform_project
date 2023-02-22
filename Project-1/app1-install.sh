4dbe25cda9cdbe1bbdaca9941f471b5856c91b02
stebible <118617126+stebible@users.noreply.github.com>
2023-02-22 16:11:29 -0500

    Update app1-install.sh

--- Project-1/app1-install.sh
+++ Project-1/app1-install.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-sudo yom update
+sudo yum update
 sudo hostnamectl set-hostname terraform-demo
 sudo su - ec2-user
 wget https://dlcdn.apache.org/maven/maven-3/3.9.0/binaries/apache-maven-3.9.0-bin.zip
