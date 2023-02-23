2fd8326c9a2d3213e4ea228e46902192346781a5
Justin Attignon <stebible@gmail.com>
2023-02-23 14:49:17 -0500

    update

--- Project-2/outputs.tf
+++ Project-2/outputs.tf
@@ -1,8 +1,8 @@
-output "aws_ami-id" {
+output "aws_ami_id" {
   value = data.aws_ami.latest-amazon-linux-image.id
 }
 
-output "ec2_public-ip" {
-  value = aws_instance.myapp-server.public.ip
+output "ec2_public_ip" {
+  value = aws_instance.myapp-server.public_ip
 }
 
